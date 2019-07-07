e5b79ea99f79a6ef8481033095799c885a81a5f0
Jhacolyte <toneth.tembrevilla@gmail.com>
2019-07-07 14:11:44 -0700

    adding bootstrap on html

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+// Global variables
+var baseAttack = 0; // original attack strength
+var player; // holds the player Object
+var defender; // holds the current defender Object
+var charArray = []; // array that stores the game characters (Objects)
+var playerSelected = false; // flag to mark if we picked a player yet
+var defenderSelected = false; // flag to mark if we picked a defender
