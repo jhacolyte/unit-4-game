b3c5544657af9728e91c7b7efe0516c43b9d1b19
Jhacolyte <toneth.tembrevilla@gmail.com>
2019-07-09 23:00:54 -0700

    finalizing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -38,7 +38,7 @@ var attackResult;
 var turnCounter = 1;
 var killCount = 0;
 
-var renderOne = function(character, renderArea, makeChar) {
+var firstRender = function(character, renderArea, makeChar) {
     var charDiv = $("<div class='character' data-name='" + character.name + "'>");
     var charName = $("<div class='character-name'>").text(character.name);
     var charImage = $("<img alt='image' class='character-image'>").attr("src", character.imageUrl);
@@ -54,7 +54,7 @@ var renderOne = function(character, renderArea, makeChar) {
     }
 };
 
-var renderMessage = function(message) {
+var showMessage = function(message) {
     var gameMesageSet = $("#gameMessage");
     var newMessage = $("<div>").text(message);
     gameMesageSet.append(newMessage);
@@ -69,26 +69,26 @@ var renderCharacters = function(charObj, areaRender) {
         $(areaRender).empty();
         for (var key in charObj) {
             if (charObj.hasOwnProperty(key)) {
-                renderOne(charObj[key], areaRender, '');
+                firstRender(charObj[key], areaRender, '');
             }
         }
     }
     if (areaRender == '#selected-character') {
-        renderOne(charObj, areaRender, '');
+        firstRender(charObj, areaRender, '');
         $('#attack-button').css('visibility', 'visible');
     }
     if (areaRender == '#available-to-attack-section') {
         $('#available-to-attack-section').prepend("Choose Your Next Opponent");
         for (var i = 0; i < charObj.length; i++) {
 
-            renderOne(charObj[i], areaRender, 'enemy');
+            firstRender(charObj[i], areaRender, 'enemy');
         }
         $(document).on('click', '.enemy', function() {
             name = ($(this).data('name'));
             if ($('#defender').children().length === 0) {
                 renderCharacters(name, '#defender');
                 $(this).hide();
-                renderMessage("clearMessage");
+                showMessage("clearMessage");
             }
         });
     }
@@ -97,24 +97,24 @@ var renderCharacters = function(charObj, areaRender) {
         for (var i = 0; i < combatants.length; i++) {
             if (combatants[i].name == charObj) {
                 $('#defender').append("Your selected opponent")
-                renderOne(combatants[i], areaRender, 'defender');
+                firstRender(combatants[i], areaRender, 'defender');
             }
         }
     }
     if (areaRender == 'playerDamage') {
         $('#defender').empty();
         $('#defender').append("Your selected opponent")
-        renderOne(charObj, '#defender', 'defender');
+        firstRender(charObj, '#defender', 'defender');
 
     }
     if (areaRender == 'enemyDamage') {
         $('#selected-character').empty();
-        renderOne(charObj, '#selected-character', '');
+        firstRender(charObj, '#selected-character', '');
     }
     if (areaRender == 'enemyDefeated') {
         $('#defender').empty();
         var gameStateMessage = "You have defeated " + charObj.name + ", you can choose to fight another enemy.";
-        renderMessage(gameStateMessage);
+        showMessage(gameStateMessage);
 
     }
 };
@@ -137,19 +137,19 @@ $(document).on('click', '.character', function() {
 $("#attack-button").on("click", function() {
     if ($('#defender').children().length !== 0) {
         var attackMessage = "You attacked " + currDefender.name + " for " + (currSelectedCharacter.attack * turnCounter) + " damage.";
-        renderMessage("clearMessage");
+        showMessage("clearMessage");
         currDefender.health = currDefender.health - (currSelectedCharacter.attack * turnCounter);
 
         if (currDefender.health > 0) {
             renderCharacters(currDefender, 'playerDamage');
             var counterAttackMessage = currDefender.name + " attacked you back for " + currDefender.enemyAttackBack + " damage.";
-            renderMessage(attackMessage);
-            renderMessage(counterAttackMessage);
+            showMessage(attackMessage);
+            showMessage(counterAttackMessage);
 
             currSelectedCharacter.health = currSelectedCharacter.health - currDefender.enemyAttackBack;
             renderCharacters(currSelectedCharacter, 'enemyDamage');
             if (currSelectedCharacter.health <= 0) {
-                renderMessage("clearMessage");
+                showMessage("clearMessage");
                 restartGame("You have been defeated...GAME OVER!!!");
                 $("#attack-button").unbind("click");
             }
@@ -157,7 +157,7 @@ $("#attack-button").on("click", function() {
             renderCharacters(currDefender, 'enemyDefeated');
             killCount++;
             if (killCount >= 3) {
-                renderMessage("clearMessage");
+                showMessage("clearMessage");
                 restartGame("You Won!!!! GAME OVER!!!");
                 setTimeout(function() {}, 2000);
                 $("#attack-button").unbind("click");
@@ -166,8 +166,8 @@ $("#attack-button").on("click", function() {
         }
         turnCounter++;
     } else {
-        renderMessage("clearMessage");
-        renderMessage("Please Select an Opponent!");
+        showMessage("clearMessage");
+        showMessage("Please Select an Opponent!");
 
     }
 });
